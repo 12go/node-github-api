cc7a663f4a09db17b0896f45dfce96cbea7e7d64
solostyle <spam@solostyle.net>
2015-06-25 12:03:10 -0500

    success! listing repos

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var gh = new Gh({ token: 'fb16ec306976da7b26936c6c4ba5f8b7d4a9521b' });
 
 var user = gh.getUser();
 
-// Promise.try(user.repos)
-//   .then(console.log)
-//   .catch(console.log);
+Promise.try(user.repos)
+  .then(console.log)
+  .catch(console.log);
 
 
-user.repos(myCb);
+// user.repos(myCb);
 
 function myCb(e, data) {
   if (!e) {
@@ -20,13 +20,3 @@ function myCb(e, data) {
     console.log('failed!', e);
   }
 }
-
-// octo.repos.create({
-//   "name": "Hello-World",
-//   "description": "This is your first repository",
-//   "homepage": "https://github.com",
-//   "private": false,
-//   "has_issues": true,
-//   "has_wiki": true,
-//   "has_downloads": true
-// });
