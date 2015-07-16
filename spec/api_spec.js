816320f94651c4fb9e8a14aa23d6235616e495c7
archanid <archana@wheelhousehq.com>
2015-07-16 17:25:17 -0600

    sanity check spec

---
+++ spec/api_spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('sanity check', function() {
+  it('is sane', function() {
+    expect(true).toBe(true);
+  });
+});
+
+describe('GH API test', function() {
+  beforeEach(function() {
+  });
+
+  it('hits the api', function() {
+  });
+});
+
